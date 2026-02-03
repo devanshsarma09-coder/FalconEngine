af69ba9957c3544b611dfa8c67d6bd794b9db993
devanshsarma09-coder <devanshsarma09@gmail.com>
2026-02-03 10:19:46 +0530

    Update Hooks.h

--- src/Hooks.h
+++ src/Hooks.h
@@ -1,12 +1,16 @@
 // src/Hooks.h
 #pragma once
-
+#include "RE/Skyrim.h"
 namespace FalconEngine {
     class Hooks {
     public:
         static void Install();
+        static void SetShipID(RE::FormID a_id);
+        static RE::FormID GetShipID();
     private:
         static void Update();
         static inline REL::Relocation<decltype(Update)> _Update;
     };
 }
+
+
