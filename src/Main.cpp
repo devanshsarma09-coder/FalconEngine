3a7cdda61302cbf407ed932d509a8d2c463310d6
devanshsarma09-coder <devanshsarma09@gmail.com>
2026-02-03 19:27:38 +0530

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,21 +2,17 @@
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 #include "Hooks.h"
-
+#include "Papyrus.h"
 using namespace RE;
 using namespace SKSE;
-
 SKSEPluginLoad(const LoadInterface* a_skse) {
     Init(a_skse);
-
-    // Allocate 14 bytes for a standard 5-byte branch hook
-    AllocTrampoline(14); 
-
+    AllocTrampoline(14);
+    GetPapyrusInterface()->Register(FalconEngine::Papyrus::Register);
     GetMessagingInterface()->RegisterListener([](MessagingInterface::Message* a_msg) {
         if (a_msg->type == MessagingInterface::kDataLoaded) {
             FalconEngine::Hooks::Install();
         }
     });
-
     return true;
 }
