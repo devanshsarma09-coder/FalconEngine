c117dd18b975b5ff3adcbe32fe2b1216644b98c8
devanshsarma09-coder <devanshsarma09@gmail.com>
2026-02-03 19:34:08 +0530

    Update Papyrus.cpp

--- src/Papyrus.cpp
+++ src/Papyrus.cpp
@@ -1,39 +1,24 @@
+// src/Papyrus.cpp
 #include "Papyrus.h"
-#include "MovementHandler.h"
+#include "Hooks.h"
 #include "AIPathing.h"
-#include "Hooks.h" // To access the ship variable
-
-namespace FalconEngine {
-    
-    // Command: FalconEngine.SetShip(ObjectReference akShip)
+namespace FalconEngine::Papyrus {
     void SetShip(RE::StaticFunctionTag*, RE::TESObjectREFR* a_ship) {
         if (!a_ship) return;
         FalconEngine::Hooks::SetShipID(a_ship->GetFormID());
     }
-
-    // Command: FalconEngine.AddCrew(Actor akActor)
     void AddCrew(RE::StaticFunctionTag*, RE::Actor* a_actor) {
         if (!a_actor) return;
-        
-        // Save where they are relative to the ship right now
-        // This locks them to their current spot on the deck
-        RE::ObjectRefHandle handle = a_actor->GetHandle();
-        
-        // We need to calculate the initial offset
-        auto ship = RE::TESForm::LookupByID<RE::TESObjectREFR>(FalconEngine::Hooks::GetShipID());
+        auto shipID = FalconEngine::Hooks::GetShipID();
+        if (shipID == 0) return;
+        auto ship = RE::TESForm::LookupByID<RE::TESObjectREFR>(shipID);
         if (ship) {
-            RE::NiPoint3 offset = a_actor->GetPosition() - ship->GetPosition();
-            RE::NiMatrix3 invRot = ship->GetRotation().Transpose();
-            
-            FalconEngine::crewLocalCoords[handle] = invRot * offset;
+            FalconEngine::AddCrewMember(a_actor, ship);
         }
     }
-
-    // Command: FalconEngine.ClearCrew()
     void ClearCrew(RE::StaticFunctionTag*) {
-        FalconEngine::crewLocalCoords.clear();
+        FalconEngine::ClearCrewList();
     }
-
     bool Register(RE::BSScript::IVirtualMachine* a_vm) {
         a_vm->RegisterFunction("SetShip", "FalconEngine", SetShip);
         a_vm->RegisterFunction("AddCrew", "FalconEngine", AddCrew);
