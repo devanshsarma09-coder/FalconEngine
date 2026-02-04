0037b55ae33e8473dfab493f98ee4b544356b484
devanshsarma09-coder <devanshsarma09@gmail.com>
2026-02-04 19:30:52 +0530

    Update Papyrus.cpp

--- src/Papyrus.cpp
+++ src/Papyrus.cpp
@@ -1,24 +1,28 @@
-// src/Papyrus.cpp
 #include "Papyrus.h"
 #include "Hooks.h"
 #include "AIPathing.h"
+
 namespace FalconEngine::Papyrus {
     void SetShip(RE::StaticFunctionTag*, RE::TESObjectREFR* a_ship) {
         if (!a_ship) return;
         FalconEngine::Hooks::SetShipID(a_ship->GetFormID());
     }
+
     void AddCrew(RE::StaticFunctionTag*, RE::Actor* a_actor) {
         if (!a_actor) return;
         auto shipID = FalconEngine::Hooks::GetShipID();
         if (shipID == 0) return;
+        
         auto ship = RE::TESForm::LookupByID<RE::TESObjectREFR>(shipID);
         if (ship) {
             FalconEngine::AddCrewMember(a_actor, ship);
         }
     }
+
     void ClearCrew(RE::StaticFunctionTag*) {
         FalconEngine::ClearCrewList();
     }
+
     bool Register(RE::BSScript::IVirtualMachine* a_vm) {
         a_vm->RegisterFunction("SetShip", "FalconEngine", SetShip);
         a_vm->RegisterFunction("AddCrew", "FalconEngine", AddCrew);
