af4161c6fdd954a9aa7851e9c976238830e2dcec
devanshsarma09-coder <devanshsarma09@gmail.com>
2026-02-04 19:28:21 +0530

    Update InputHandler.h

--- src/InputHandler.h
+++ src/InputHandler.h
@@ -1,9 +1,10 @@
-
-// src/InputHandler.h
 #pragma once
 #include "RE/Skyrim.h"
+
 namespace FalconEngine {
+    // These must be defined as extern to be seen by MovementHandler
     extern RE::NiPoint3 localPos;
     extern float speed;
+    
     void HandleInput();
 }
