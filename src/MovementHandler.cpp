5fd6bd1b59aea63d4d2429b2177a8fe6db2fff5c
devanshsarma09-coder <devanshsarma09@gmail.com>
2026-02-04 19:26:56 +0530

    Update MovementHandler.cpp

--- src/MovementHandler.cpp
+++ src/MovementHandler.cpp
@@ -1,13 +1,19 @@
 #include "MovementHandler.h"
 #include "InputHandler.h"
+
 namespace FalconEngine {
     void ApplyMovement(RE::Actor* a_actor, RE::TESObjectREFR* a_ship) {
         if (!a_actor || !a_ship) return;
+        
         auto root = a_ship->Get3D();
         if (!root) return;
+
         RE::NiMatrix3 shipRot = root->world.rotate;
         RE::NiPoint3 shipPos = root->world.translate;
+        
+        // global localPos from InputHandler
         RE::NiPoint3 worldPos = shipPos + (shipRot * localPos);
+        
         a_actor->SetPosition(worldPos, true);
         
         float rX, rY, rZ;
@@ -15,4 +21,3 @@ namespace FalconEngine {
         a_actor->SetRotation(rX, rY, rZ);
     }
 }
-
